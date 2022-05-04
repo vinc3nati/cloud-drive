e4db70d889b0a5728cd05f7b8ef19ad523593d9d
Vinit Kanse <vinitkansend@gmail.com>
2022-05-04 16:49:05 +0530

    Fix: API keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,21 +4,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const app = firebase.initializeApp({
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  apiKey: "AIzaSyAb3FsAjXFslWHD7SIK2SIxu9qs2-B4qiM",
-  authDomain: "drive-clone-e4f9b.firebaseapp.com",
-  databaseURL: "https://drive-clone-e4f9b.firebaseio.com",
-  projectId: "drive-clone-e4f9b",
-  storageBucket: "drive-clone-e4f9b.appspot.com",
-  messagingSenderId: "665329843352",
-  appId: "1:665329843352:web:3ff07d34211a29050364ed",
-  measurementId: "G-WSH4DJH314",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 });
 
 const firestore = app.firestore();
